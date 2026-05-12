ae8674944af0f1198f58d2308a048c58ea1b8994
Manoj Sunar <mutipletask@gmail.com>
2026-05-12 11:26:23 +0545

    fix

--- screen/app/API/apiClient.ts
+++ screen/app/API/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL||"http://localhost:5000",
     withCredentials: true,
     timeout: 60000,
 
